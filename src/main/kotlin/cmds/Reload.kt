8462b7a0ebeea35e1d204c6f05fb2f3739bf94b0
David Mak <david.18.19.21@gmail.com>
2018-03-05 09:32:48 +0800

    Reload: Add help text

--- src/main/kotlin/cmds/Reload.kt
+++ src/main/kotlin/cmds/Reload.kt
@@ -1,9 +1,11 @@
 package cmds
 
+import core.BuilderHelper
 import core.Core
 import core.IChannelLogger
 import core.Parser
 import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent
+import sx.blah.discord.util.DiscordException
 
 object Reload : IBase, IChannelLogger {
     override fun handlerSu(event: MessageReceivedEvent): Parser.HandleState {
@@ -14,5 +16,24 @@ object Reload : IBase, IChannelLogger {
         }
 
         return Parser.HandleState.HANDLED
+    }
+
+    override fun help(event: MessageReceivedEvent, isSu: Boolean) {
+        try {
+            BuilderHelper.buildEmbed(event.channel) {
+                withTitle("Help Text for `reload`")
+                withDesc("Reloads essential bot properties from their respective files.")
+                appendField("\u200B", "\u200B", false)
+                appendField("Usage", "```reload```", false)
+                withFooterText("Package: ${this@Reload.javaClass.name}")
+            }
+        } catch (e: DiscordException) {
+            log(IChannelLogger.LogLevel.ERROR, "Cannot display help text") {
+                author { event.author }
+                channel { event.channel }
+                info { e.errorMessage }
+            }
+            e.printStackTrace()
+        }
     }
 }
