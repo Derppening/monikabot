10a239c2013024ea706d4ef7ac370324772a0e82
David Mak <david.18.19.21@gmail.com>
2018-03-11 21:44:34 +0800

    Warframe: Initial addition of data humanizers

--- src/main/kotlin/cmds/Warframe.kt
+++ src/main/kotlin/cmds/Warframe.kt
@@ -5,6 +5,7 @@ import cmds.warframe.News
 import cmds.warframe.WorldState
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.MapperFeature
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import core.BuilderHelper.buildEmbed
@@ -56,6 +57,117 @@ object Warframe : IBase, IChannelLogger, IConsoleLogger {
         }
     }
 
+    object Unobfuscate {
+        internal fun getArcaneInfo(regex: String): Arcane {
+            return try {
+                jacksonObjectMapper().apply {
+                    configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                    configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
+                }.readValue<List<Arcane>>(URL("$worldStateDataUrl/arcanes.json"))
+                        .find { it.regex == regex || it.name == regex } ?: Arcane()
+            } catch (e: Exception) {
+                Arcane()
+            }
+        }
+
+        internal fun getFactionString(faction: String): String {
+            return try {
+                jacksonObjectMapper().apply {
+                    configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                    configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
+                }.readTree(URL("$worldStateDataUrl/factionsData.json"))
+                        .get(faction)
+                        .get("value").asText()
+            } catch (e: Exception) {
+                ""
+            }
+        }
+
+        internal fun getFissureModifier(tier: String): String {
+            return try {
+                jacksonObjectMapper().apply {
+                    configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                    configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
+                }.readTree(URL("$worldStateDataUrl/fissureModifiers.json"))
+                        .get(tier)
+                        .get("value").asText()
+            } catch (e: Exception) {
+                ""
+            }
+        }
+
+        internal fun getLanguageFromAsset(encoded: String): String {
+            return try {
+                jacksonObjectMapper().apply {
+                    configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                    configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
+                }.readTree(URL("$worldStateDataUrl/languages.json"))
+                        .get(encoded)
+                        .get("value").asText()
+            } catch (e: Exception) {
+                ""
+            }
+        }
+
+        internal fun getMissionType(missionType: String): String {
+            return try {
+                jacksonObjectMapper().apply {
+                    configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                    configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
+                }.readTree(URL("$worldStateDataUrl/missionTypes.json"))
+                        .get(missionType)
+                        .get("value").asText()
+            } catch (e: Exception) {
+                ""
+            }
+        }
+
+        internal fun getSolNode(solNode: String): SolNode {
+            return try {
+                jacksonObjectMapper().apply {
+                    configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                    configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
+                }.readTree(URL("$worldStateDataUrl/solNodes.json"))
+                        .get(solNode)
+                        .let {
+                            ObjectMapper().readValue(it.toString())
+                        }
+            } catch (e: Exception) {
+                SolNode()
+            }
+        }
+
+        // TODO(Derppening): getSortie...
+
+        internal fun getSyndicateName(syndicate: String): String {
+            return try {
+                jacksonObjectMapper().apply {
+                    configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                    configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
+                }.readTree(URL("$worldStateDataUrl/syndicatesData.json"))
+                        .get(syndicate)
+                        .get("value").asText()
+            } catch (e: Exception) {
+                ""
+            }
+        }
+
+        internal fun getUpgradeType(upgrade: String): String {
+            return try {
+                jacksonObjectMapper().apply {
+                    configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                    configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
+                }.readTree(URL("$worldStateDataUrl/upgradeTypes.json"))
+                        .get(upgrade)
+                        .get("value").asText()
+            } catch (e: Exception) {
+                ""
+            }
+        }
+
+        // TODO(Derppening): getWarframe... and getWeapon...
+    }
+
     private fun updateWorldState() {
         if (!Client.isReady) {
             return
@@ -70,9 +182,25 @@ object Warframe : IBase, IChannelLogger, IConsoleLogger {
 
         logger.debug("updateWorldState(): Parse WorldState took ${timer}ms")
     }
+    class Arcane {
+        val regex = ""
+        val name = ""
+        val effect = ""
+        val rarity = ""
+        val location = ""
+        val thumbnail = ""
+        val info = ""
+    }
+
+    class SolNode {
+        val value = ""
+        val enemy = ""
+        val type = ""
+    }
 
     val updateWorldStateTask = timer("Update WorldState Timer", true, 0, 30000) { updateWorldState() }
 
+    private const val worldStateDataUrl = "https://raw.githubusercontent.com/WFCD/warframe-worldstate-data/master/data"
     private const val worldStateUrl = "http://content.warframe.com/dynamic/worldState.php"
 
     var worldState = WorldState()
