e2bd37cb6e44753d111b49b61775844f88c2161e
David Mak <david.18.19.21@gmail.com>
2018-03-11 21:05:28 +0800

    Debug: Remove redundant fields

--- src/main/kotlin/cmds/Debug.kt
+++ src/main/kotlin/cmds/Debug.kt
@@ -1,17 +1,10 @@
 package cmds
 
 import core.BuilderHelper.buildEmbed
-import core.BuilderHelper.buildMessage
-import core.Core.getChannelName
-import core.Core.getDiscordTag
 import core.IChannelLogger
 import core.Parser
-import core.PersistentMessage
 import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent
 import sx.blah.discord.util.DiscordException
-import java.awt.Color
-import java.time.LocalDateTime
-import java.time.ZoneId
 
 /**
  * Singleton handling "debug" commands.
@@ -26,51 +19,6 @@ object Debug : IBase, IChannelLogger {
         }
 
         when (args[0].toLowerCase()) {
-            "buildembed" -> run {
-                buildEmbed(event.channel) {
-                    withTitle("Title")
-                    withDesc("Description")
-                    withAuthorName("Author Name")
-                    withColor(Color.RED)
-                    withTimestamp(LocalDateTime.now(ZoneId.of("UTC")))
-                    appendField("Field Key", "Field Content", false)
-                    withFooterText("Footer Text")
-                }
-            }
-            "log" -> run {
-                buildEmbed(event.channel) {
-                    withColor(Color.RED)
-                    withTitle("Error")
-                    withDesc("Cannot display help text")
-                    appendField("\u200B", "\u200B", false)
-                    appendField("Caused by", "`${event.message.content}`", false)
-                    appendField("From", getDiscordTag(event.author), false)
-                    appendField("In", getChannelName(event.channel), false)
-                    appendField("Additional Info", ":(", false)
-                }
-            }
-            "persist" -> run {
-                if (args.size == 2 && args[1].matches("-{0,2}help".toRegex())) {
-                    buildEmbed(event.channel) {
-                        withTitle("Help Text for `debug-persist`")
-                        withDesc("Directly modifies persistence text.")
-                        appendField("\u200B", "\u200B", false)
-                        appendField("Usage", "```debug persist [header] [key] [value]```", false)
-                        appendField("`[header]`", "Header to put the key/value pair", false)
-                        appendField("`[key]`", "Name of the value", false)
-                        appendField("`[value]`", "Value", false)
-                    }
-                }
-                if (args.size != 4) { return@run }
-                PersistentMessage.modify(args[1], args[2], args[3], true)
-            }
-            "warframe-language" -> run {
-                if (args.size != 2) { return@run }
-
-                buildMessage(event.channel) {
-                    withContent(cmds.experimental.Warframe.getLanguageFromAsset(args[1]))
-                }
-            }
             else -> {
                 log(IChannelLogger.LogLevel.ERROR, "Unknown debug option \"${args[0]}\"") {
                     message { event.message }
