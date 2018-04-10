4b026f7c53a59e75ed3bce0e72df5549b06a5689
David Mak <david.18.19.21@gmail.com>
2018-04-10 15:19:54 +0800

    Update copyright text

--- src/main/kotlin/cmds/experimental/Emoticon.kt
+++ src/main/kotlin/cmds/experimental/Emoticon.kt
@@ -1,3 +1,23 @@
+/*
+ *  This file is part of MonikaBot.
+ *
+ *  Copyright (C) 2018 Derppening <david.18.19.21@gmail.com>
+ *
+ *  MonikaBot is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  MonikaBot is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with MonikaBot.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package cmds.experimental
 
 import cmds.IBase
