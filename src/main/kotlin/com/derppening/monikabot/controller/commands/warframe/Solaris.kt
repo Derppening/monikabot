9b74a8fd9fba4efa472bd4e44c386e21a8a8d5e2
David Mak <david.18.19.21@gmail.com>
2018-11-10 02:05:03 +0800

    [warframe-solaris] Initial commit

---
+++ src/main/kotlin/com/derppening/monikabot/controller/commands/warframe/Solaris.kt
@@ -0,0 +1,56 @@
+/*
+ *  This file is part of MonikaBot.
+ *
+ *  Copyright (C) 2018 Derppening <david.18.19.21@gmail.com>
+ *
+ *  MonikaBot is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  MonikaBot is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with MonikaBot.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.derppening.monikabot.controller.commands.warframe
+
+import com.derppening.monikabot.controller.CommandInterpreter
+import com.derppening.monikabot.controller.commands.IBase
+import com.derppening.monikabot.core.ILogger
+import com.derppening.monikabot.impl.warframe.SolarisService
+import com.derppening.monikabot.util.helpers.EmbedHelper.sendEmbed
+import com.derppening.monikabot.util.helpers.HelpTextBuilder.buildHelpText
+import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent
+
+object Solaris : IBase, ILogger {
+    override fun cmdName(): String = "warframe-solaris"
+
+    override fun handler(event: MessageReceivedEvent): CommandInterpreter.HandleState {
+        val args = getArgumentList(event.message.content).drop(1)
+
+        when {
+            args.isEmpty() -> displayBounties(event)
+            else -> help(event, false)
+        }
+
+        return CommandInterpreter.HandleState.HANDLED
+    }
+
+    private fun displayBounties(event: MessageReceivedEvent) {
+        SolarisService.getBountyEmbeds().forEach {
+            sendEmbed(it to event.channel)
+        }
+    }
+
+    override fun help(event: MessageReceivedEvent, isSu: Boolean) {
+        buildHelpText(cmdName(), event) {
+            description { "Displays Solaris-related information" }
+        }
+    }
+}
